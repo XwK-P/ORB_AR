262c8c8d373c2c9da92a3582c28ce0658bea236a
XwK-P <pauliwang411@gmail.com>
2016-04-29 22:47:45 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ int main( void )
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 640, 480, "OpenGL Demo", NULL, NULL);
+    window = glfwCreateWindow( 640, 480, "ORB AR", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
